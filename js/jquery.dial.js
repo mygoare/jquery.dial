db132259e6810c58b3af29e2a018e612420a6663
mygoare <mygoare@gmail.com>
2014-09-04 17:45:25 +0800

    first commit

---
+++ js/jquery.dial.js
@@ -0,0 +1,286 @@
+/**
+ * @name		jQuery Dial plugin
+ * @author		Martin Angelov
+ * @version 	1.0
+ * @url			http://tutorialzine.com/2011/11/pretty-switches-css3-jquery/
+ * @license		MIT License
+ */
+
+/*
+
+ Modified by Mesheven, repo url: https://github.com/mygoare/KnobKnob
+
+ Optimize the plugin based on: http://www.websanova.com/blog/jquery/10-coding-tips-to-write-superior-jquery-plugins#.UwsRSUKSxgs
+
+ Some updates:
+ 1.  Make the plugin with OOP mode
+ 2.  Add two more themes
+ 3.  add `rotate` method to change the rotate value dynamic
+
+ 4.  add angleOffset, angleArc, min, max parameters
+ 5.  this.options.$ stands for $(element)
+
+ 6.  options.piece make the snap feature
+ 7.  options.moveSensitivity
+
+ */
+
+(function($){
+
+    function Dial(props, settings)
+    {
+        this.dial = null;
+        this.dialTop = null;
+        this.currentDeg = 0;
+        this.v = 0;
+        this.doc = $(document);
+        this.el = null;
+
+        // moveOrientation horizontal or vertical
+        this.pos = 0;
+
+        if (typeof props == 'object')
+        {
+            settings = props;
+        }
+
+        this.options = $.extend({}, $.fn.dial.defaultSettings, settings || {});
+
+        this.snap = this.options.angleArc / ( this.options.piece || (this.options.max - this.options.min) );
+    }
+
+    Dial.prototype =
+    {
+        generate: function(el)
+        {
+            var tpl = '<div class="dial">\
+                           <div class="top"></div>\
+                           <div class="base"></div>\
+                       </div>';
+
+            el.append(tpl);
+            this.dial = $('.dial', el);
+            this.dialTop = this.dial.find('.top');
+            this.dial.addClass('dial-' + this.options.className);
+            this.el = this.options.$ = el;
+
+            this.init(this.options.value);
+        },
+        init: function(v)
+        {
+            if (typeof v !== 'number' && v < this.options.min && v > this.options.max)
+            {
+                return false;
+            }
+
+            // v calculate out d
+            this.v = v;
+            var d = (v - this.options.min) / (this.options.max - this.options.min) * this.options.angleArc + this.options.angleOffset;
+
+            this.currentDeg = d; // init need
+            this.rotate(d);
+
+            // 给第三方初始化使用
+            this.options.init(v / (this.options.max - this.options.min));
+        },
+        rotate: function(d)
+        {
+            if (d < 0)
+            {
+                d = d + 360;
+            }
+
+            this.dialTop.css('transform','rotate('+(d)+'deg)');
+        },
+        calVal: function()
+        {
+            this.v = (this.currentDeg - this.options.angleOffset) / this.options.angleArc * (this.options.max - this.options.min) + this.options.min;
+            this.v = Math.round(this.v);
+        },
+        clickRotate: function(e, rad2deg, offset, endDeg)
+        {
+            e = (e.originalEvent.touches) ? e.originalEvent.touches[0] : e;
+
+            var moveOrientation = this.options.moveOrientation,
+                deg;
+            if (moveOrientation === "rotate")
+            {
+                var a, b,
+                    p = this.dial.offset(),  // need real time p and center value
+                    center =
+                    {
+                        y: p.top + this.dial.height() / 2,
+                        x: p.left + this.dial.width() / 2
+                    };
+
+                a = center.y - e.pageY;
+                b = center.x - e.pageX;
+                deg = Math.atan2(a,b)*rad2deg;  // -180 ~ 180 degree
+//console.error(center, e.pageX, e.pageY, deg);
+
+                var buffer = (360 - this.options.angleArc) / 2;
+                // console.error(deg);
+                if (deg > -180 && deg < endDeg - 360 + buffer)  // 加点buffer 给endDeg 边界 一些缓冲, 如果angleArc 为360，即全圆转圈时，buffer计算得出为 0
+                {
+                    deg = 180 + (180 - Math.abs(deg));
+                }
+            }
+            else if (moveOrientation === "horizontal")
+            {
+                deg = this.currentDeg;
+                deg += (e.pageX - this.pos) * this.snap / Math.abs(this.options.moveSensitivity);
+                this.pos = e.pageX;
+            }
+            else if (moveOrientation === "vertical")
+            {
+                deg = this.currentDeg;
+                console.log(this.currentDeg);
+                deg += (e.pageY - this.pos) * this.snap / Math.abs(this.options.moveSensitivity);
+                this.pos = e.pageY;
+            }
+
+            // handle with Boundary of two sides
+            if (deg > endDeg)
+            {
+                deg = endDeg;
+            }
+            else if (deg < offset)
+            {
+                deg = offset;
+            }
+
+            this.currentDeg = deg;
+
+            // rotate top decoration degree
+            this.rotate(this.currentDeg);
+
+            // calculate value
+            this.calVal();
+            this.options.turn(this.v, this.currentDeg);
+        },
+        bind: function()
+        {
+            var _this = this;
+
+            // define variables
+            var rad2deg = 180 / Math.PI,
+                offset = this.options.angleOffset,
+                arc = this.options.angleArc,
+                endDeg = offset + arc;
+
+            this.dial.on('mousedown touchstart', function(e)
+            {
+                e.preventDefault();
+                e = e.originalEvent.touches ? e.originalEvent.touches[0] : e;
+
+                switch (_this.options.moveOrientation)
+                {
+                    case "rotate":
+                        _this.clickRotate(e, rad2deg, offset, endDeg);
+                        break;
+                    case "horizontal":
+                        _this.pos = e.pageX;
+                        break;
+                    case "vertical":
+                        _this.pos = e.pageY;
+                        break;
+                }
+
+                _this.doc.on('mousemove.rem touchmove.rem',function(e)
+                {
+                    _this.clickRotate(e, rad2deg, offset, endDeg);
+                });
+
+                _this.doc.on('mouseup.rem  touchend.rem',function()
+                {
+                    _this.doc.off('.rem');
+
+                    _this.options.change(_this.v, _this.currentDeg);
+                });
+
+            });
+
+            // mouse wheel support
+            if (_this.options.mouseWheel === true)
+            {
+                this.dial.on("DOMMouseScroll mousewheel", function(e)
+                {
+                    e.preventDefault();
+                    var snap = _this.snap;
+
+                    if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0)
+                    {
+                        _this.currentDeg -= snap;
+                    }
+                    else
+                    {
+                        _this.currentDeg += snap;
+                    }
+
+                    // handle with Boundary of two sides
+                    if (_this.currentDeg > endDeg)
+                    {
+                        _this.currentDeg = endDeg;
+                    }
+                    else if (_this.currentDeg < offset)
+                    {
+                        _this.currentDeg = offset;
+                    }
+
+                    // rotate top decoration degree
+                    _this.rotate(_this.currentDeg);
+
+                    // calculate value
+                    _this.calVal();
+                    _this.options.turn(_this.v, _this.currentDeg);
+
+                    _this.options.change(_this.v, _this.currentDeg);
+                });
+            }
+
+        },
+        destroy: function()
+        {
+            this.el.empty().removeData('dial');
+            delete this.el[0].el;
+        }
+    };
+
+    $.fn.dial = function(props, settings)
+    {
+        return this.each(function()
+        {
+            var dial = new Dial(props, settings);
+
+            if (!this.el)
+            {
+                this.el = $(this);
+                dial.generate(this.el);
+                dial.bind();
+
+                this.el.data('dial', dial);
+            }
+        });
+    };
+
+    $.fn.dial.defaultSettings =
+    {
+        min            : 0,
+        max            : 100,
+        angleOffset    : 0,  // it is a value between -180 ~ 180
+        angleArc       : 360,
+        className      : "default",
+        value          : 0,
+        turn           : function (value, deg) {
+        },
+        change         : function (value, deg) {
+        },
+        init           : function (percent) {
+
+        },  // do something third part needs
+        moveOrientation: "rotate",  // horizontal, vertical
+        mouseWheel     : true,
+        moveSensitivity: 20  // number smaller, changes moved more sensitive, must greater than 0
+    };
+
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,7 @@
+Optimize the plugin based on: [http://www.websanova.com/blog/jquery/10-coding-tips-to-write-superior-jquery-plugins#.UwsRSUKSxgs](/Users/mygoare/Projects/mesheven/mesh-browser-app/public/vendor/jquery.dial/css/Users/mygoare/Projects/mesheven/mesh-browser-app/pu
+blic/vendor/jquery.dial/images/Users/mygoare/Projects/mesheven/mesh-br
+owser-app/public/vendor/jquery.dial/js)
+
+## Usage:
+
+    $('#elem').dial();
